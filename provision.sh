91e3c2dde5aa80a51424371d960bf37e11622679
Josh Farwell <josh.farwell@gmail.com>
2017-11-05 14:23:21 -0800

    Updated provisioning script again
    Split it out, ditched submodules, added Digital Ocean provisioner
    
    Signed-off-by: Josh Farwell <josh.farwell@gmail.com>

--- provision.sh
+++ provision.sh
@@ -29,7 +29,3 @@ apt-get -f install -y
 
 # Android Kernel build dependencies
 apt-get install -y git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip python-networkx
-
-
-# Download sources, see .gitmodules
-git submodule init && git submodule update
